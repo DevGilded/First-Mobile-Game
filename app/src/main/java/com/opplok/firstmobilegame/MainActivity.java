704204ba8abccf829331acd221b6de41eb4c82ad
DevGilded <goldencyco@gmail.com>
2024-07-05 11:46:05 +0800

    Game Loop

--- app/src/main/java/com/opplok/firstmobilegame/MainActivity.java
+++ app/src/main/java/com/opplok/firstmobilegame/MainActivity.java
@@ -3,6 +3,8 @@ package com.opplok.firstmobilegame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+
+        setContentView(new Game(this));
     }
 }
